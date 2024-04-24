27e1a834a38332bb77c910f31332fc65ee942ed5
Agil Fadillah Sabri <agilfadillahsabri03@gmail.com>
2024-04-24 07:50:01 +0700

    refactor: refactoring dfs.go

--- src/scrapper/ids.go
+++ src/scrapper/ids.go
@@ -1,41 +1,43 @@
 package scrapper
 
 /**
- * Function to perform Depth First Search (DFS) algorithm
+ * Function to perform Depth Limited Search (DLS) algorithm
  *
- * @param current_node: the current node (current link)
- * @param link_tujuan: title of the destination link
+ * @param currentNode: the current node (current link)
+ * @param destinationLink: link of the destination link
  * @param depth: the maximum depth to search
  * @param hasil: a list of nodes that contain the path from the start node to the destination node
  */
- func depth_limited_search(current_node Node,link_tujuan string, depth int, hasil *[]Node){
-	if(depth >= 0){
-		if current_node.Current == link_tujuan{
-			current_node.Paths = append(current_node.Paths, current_node.Current)
-			*hasil = append(*hasil, current_node)
+func depth_limited_search(currentNode Node, destinationLink string, depth int, hasil *[]Node) {
+	if depth >= 0 {
+		if currentNode.Current == destinationLink {
+			currentNode.Paths = append(currentNode.Paths, currentNode.Current)
+			*hasil = append(*hasil, currentNode)
 		} else {
-			tetangga := getAdjacentLinks(current_node)
-			for _,node := range tetangga {
-				if node.Current == link_tujuan {
-					current_node.Paths = append(current_node.Paths, current_node.Current)
-					*hasil = append(*hasil, current_node)
-				} else {
-					depth_limited_search(node,link_tujuan,depth-1,hasil)
-				}
+			tetangga := getAdjacentLinks(currentNode)
+			for _, node := range tetangga {
+				depth_limited_search(node, destinationLink, depth-1, hasil)
 			}
 		}
 	}
 }
 
-func iterative_deepening_search(link_awal string, link_tujuan string,hasil *[]Node){
+/**
+ * Function to perform Iterative Deepening Search (IDS) algorithm
+ *
+ * @param startLink: the start link
+ * @param destinationLink: link of the destination link
+ * @param hasil: a list of nodes that contain the path from the start node to the destination node
+ */
+func iterative_deepening_search(startLink string, destinationLink string, hasil *[]Node) {
 	var initial Node
-	initial.Current = link_awal
+	initial.Current = startLink
 
 	var solutions []Node
 	depth := 0
 
-	for (len(solutions) == 0) {
-		depth_limited_search(initial, link_tujuan, depth,&solutions)
+	for len(solutions) == 0 {
+		depth_limited_search(initial, destinationLink, depth, &solutions)
 		depth++
 	}
 
